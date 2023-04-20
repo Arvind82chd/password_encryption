4d6b5f1364561a688b6c655bcab0dfb4db5daae5
Arvind82chd <58764578+Arvind82chd@users.noreply.github.com>
2023-04-19 19:57:38 -0700

    converting the code into module

--- crud.rb
+++ crud.rb
@@ -1,40 +1,28 @@
-require 'bcrypt'
+module Crud
+  require 'bcrypt'
 
-users = [
-    {username: "a", password: "pwd"},
-    {username: "b", password: "pwd1"},
-    {username: "c", password: "pwd2"},
-    {username: "d", password: "000"},
-    {username: "e", password: "111"}
-]
-
-
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
-
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
-
-def create_secure_users(list_of_users)
-  list_of_users.each do |user_record|
-    user_record[:password] = create_hash_digest(user_record[:password])
+  def create_hash_digest(password)
+    BCrypt::Password.create(password)
   end
-  list_of_users
-end
 
+  def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
 
-create_secure_users(users)
-
-def authenticate_user(username, password, list_of_users)
-  list_of_users.each do |user_record|
-    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
-      puts "username and password matched"
-      return user_record
+  def create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
     end
+    list_of_users
   end
-  "Wrong credentials"
-end
 
-puts authenticate_user('d', '000', users)
+  def authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+        puts "username and password matched"
+        return user_record
+      end
+    end
+    "Wrong credentials"
+  end
+end
---
+++ main.rb
@@ -0,0 +1,6 @@
+require_relative 'crud'
+
+# or 
+
+$LOAD_PATH << "." # If we are in the same directory.
+require 'crud' # if we are not in the same directory, this could be use directly.

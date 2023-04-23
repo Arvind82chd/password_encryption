6d0c5db4d29f5d0670e51bf90c1431035a2d51d6
Arvind Sharma <arvindsharma@Arvinds-MacBook-Air.local>
2023-04-23 08:59:19 -0700

    adding student class

---
+++ student.rb
@@ -0,0 +1,24 @@
+require_relative 'crud'
+
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, User name: #{@username}, Email: #{@email}, Password: #{@password}"
+  end
+  
+end
+
+student1 = Student.new("Ari", "Hussainpuria", "ari_h", "ari@abc.com", "pwd1")
+student2 = Student.new("Chaman", "Basantpuria", "chaman", "c@abc.com", "pwd2")
+
+puts student1
+puts student2
